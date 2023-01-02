27d858af502d96f1621b477cb57699da24c1332e
Ethan Thomure <ethanthomure@icloud.com>
2023-01-02 09:23:57 -0600

    Stage 3

---
+++ Coffee Machine/task/main.js
@@ -0,0 +1,18 @@
+const input = require('sync-input');
+
+const waterPerCoffeeCup = 200;
+const milkPerCoffeeCup = 50;
+const coffeeBeenPerCoffeeCup = 15;
+
+const coffeeCoefficient = [Number(input("Write how many ml of water the coffee machine has:\n")) / waterPerCoffeeCup,
+    Number(input("Write how many ml of water the coffee machine has:\n")) / milkPerCoffeeCup,
+    Number(input("Write how many grams of coffee beans the coffee machine has:\n")) / coffeeBeenPerCoffeeCup];
+smallestCoffeeCoefficient = Math.floor(Math.min.apply(Math, coffeeCoefficient));
+const neededCupsOfCoffee = Number(input("Write how many cups of coffee you will need:\n"));
+
+if (smallestCoffeeCoefficient >= neededCupsOfCoffee) {
+    console.log(`Yes, I can make that amount of coffee${(smallestCoffeeCoefficient === neededCupsOfCoffee) 
+                ? "" : ` (and even ${smallestCoffeeCoefficient - neededCupsOfCoffee} more than that`}`);
+} else {
+    console.log(`No, I can make only ${smallestCoffeeCoefficient} cups of coffee`);
+}
