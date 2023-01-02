1135de34a08b3c4471b53181b8b0f91638265ffa
Ethan Thomure <ethanthomure@icloud.com>
2023-01-02 12:20:09 -0600

    Stage 5

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -1,56 +1,76 @@
 const input = require('sync-input');
 
-let supplies = {water: 400, milk: 540, coffeeBeans: 120, cups: 9, money: 550,
-    displayContents() {
-        console.log(`The coffee machine has:
-${supplies.water} ml of water
-${supplies.milk} ml of milk
-${supplies.coffeeBeans} g of coffee beans
-${supplies.cups} disposable cups
-$${supplies.money} of money`);
-    }};
+let supplies = {water: 400, milk: 540, "coffee beans": 120, cups: 9};
+let money = 550;
 
 const coffeeIngredients = {
-    1: {water: 250, milk: 0, coffeeBeans: 16, cost: 4},
-    2: {water: 350, milk: 75, coffeeBeans: 20, cost: 7},
-    3: {water: 200, milk: 100, coffeeBeans: 12, cost: 6}
+    1: {water: 250, milk: 0, "coffee beans": 16, cups: 1, cost: 4},
+    2: {water: 350, milk: 75, "coffee beans": 20, cups: 1, cost: 7},
+    3: {water: 200, milk: 100, "coffee beans": 12, cups: 1, cost: 6}
 };
 
+function displayContents() {
+    console.log(`
+The coffee machine has:
+${supplies.water} ml of water
+${supplies.milk} ml of milk
+${supplies["coffee beans"]} g of coffee beans
+${supplies.cups} disposable cups
+$${money} of money
+`);
+}
+
 function buy() {
-    let choice = Number(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n"));
+    let choice = input("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n");
+    if (choice === "back") {
+        return;
+    } else {
+        choice = Number(choice);
+    }
+    for (let i in supplies) {
+        if (Math.floor(supplies[i] / coffeeIngredients[choice][i]) === 0) {
+            console.log(`Sorry, not enough ${i}!\n`);
+            return;
+        }
+    }
+    console.log("I have enough resources, making you a coffee!");
     supplies.water -= coffeeIngredients[choice].water;
     supplies.milk -= coffeeIngredients[choice].milk;
-    supplies.coffeeBeans -= coffeeIngredients[choice].coffeeBeans;
+    supplies["coffee beans"] -= coffeeIngredients[choice]["coffee beans"];
     supplies.cups--;
-    supplies.money += coffeeIngredients[choice].cost;
+    money += coffeeIngredients[choice].cost;
+
     console.log()
 }
 
 function fill() {
-    supplies.water += Number(input("Write how many ml of water you want to add:\n"));
+    supplies.water += Number(input("\nWrite how many ml of water you want to add:\n"));
     supplies.milk += Number(input("Write how many ml of milk you want to add:\n"));
-    supplies.coffeeBeans += Number(input("Write how many grams of coffee beans you want to add:\n"));
+    supplies["coffee beans"] += Number(input("Write how many grams of coffee beans you want to add:\n"));
     supplies.cups += Number(input("Write how many disposable cups you want to add:\n"));
     console.log()
 }
 
 function take() {
-    console.log(`I gave you $${supplies.money}`);
-    supplies.money = 0;
+    console.log(`
+I gave you $${money}`);
+    money = 0;
 }
 
-supplies.displayContents();
-switch (input("Write action (buy, fill, take):\n")) {
-    case "buy":
-        buy();
-        supplies.displayContents();
-        break;
-    case "fill":
-        fill();
-        supplies.displayContents();
-        break;
-    case "take":
-        take();
-        supplies.displayContents();
-        break;
-}
+let action;
+do {
+    action = input("Write action (buy, fill, take, remaining, exit):\n")
+    switch (action) {
+        case "buy":
+            buy();
+            break;
+        case "fill":
+            fill();
+            break;
+        case "take":
+            take();
+            break;
+        case "remaining":
+            displayContents();
+    }
+} while (action !== "exit");
